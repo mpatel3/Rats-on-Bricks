d229e34ee4aadf9f65b702484f510b9fdd29e502
Manthan Patel <manthanp@cybage.com>
2016-09-10 23:13:42 +0530

    comments added into the code.

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
-/*Declaring Array */
-var temp_array = [];  /*Array for storing the Temprature Values */
-var input_arr = []; /*Array for storing the Rats positions*/
-var min_array = []; /*Final Rats Positions*/
+
+/*------------------------------DECLARING ARRAYS -----------------------------------------------------------------*/
+
+var temp_array = [];    /*ARRAY FOR STORING THE TEMPRATUE VALUES*/
+var input_arr = [];    /*ARRAY FOR STORING THE RATS POSITIONS AND FINAL OUTPUT*/
+var min_array = [];    /*RATS POSITION ARRAY USED FOR INTERMIDIATE PHASES*/
 
 function RatsPostions(input1,input2,input3)
 { 
 	temp_array = input2.split(',');
         
-        /*Creating a 2 dimension array for the temprature*/
+        /*CREATING 2 DIMENSION ARRAY FOR THE TEMPRATURE*/
+        
         for(var k=0;k<temp_array.length;k++)
         {
             temp_array.push(temp_array[0].split('#'));
@@ -15,24 +18,26 @@ function RatsPostions(input1,input2,input3)
            
         }
         
-        /*Creating a 2 dimensional array for rat positions*/
+        /*CREATING 2 DIMENSION ARRAY FOR RATS POSITIONS*/
         
         input_arr = input1.split(',');
         
-        for(var p=0;p<input_arr.length;p++)  // position array
+        for(var p=0;p<input_arr.length;p++)  
         {
             input_arr.push(input_arr[0].split('#'));
             input_arr.shift();
         }
        
-       /*Function to find the Minimum Magnitude from the current rat position and that bricks's dimension.*/
+       /* FUNCTION TO FIND THE MINIMUM MAGNITUDE(TEMPRATURE DIFFERENCE) AND THE CURRENT RAT POSITIONS (i.e. BRICK DIMENSION) */
+        
         function findMin(pos1,pos2) {
     
           var  Min;
           var  obj  = [pos1,pos2];
           var count = 0;
            
-            /* Checking Top position from current brick */
+            /* CHECK TOP POSITION FROM THE CURRENT BRICK */
+            
             if( pos1-1 >= 0 && pos2 >= 0) {
  
                 Min = Math.abs(temp_array[pos1][pos2] - temp_array[pos1-1][pos2]); 
@@ -41,7 +46,8 @@ function RatsPostions(input1,input2,input3)
                 
             } 
                        
-            /* Checking right position from current brick */
+            /* CHECK RIGHT POSITION FROM THE CURRENT BRICK */
+            
             if(pos1 >= 0 && pos2+1 < 5) {
 
               if( Min != undefined ) {
@@ -63,11 +69,10 @@ function RatsPostions(input1,input2,input3)
                 }
             }  
 	   
-	    /* Checking Bottom position from current brick */	                   
+	    /* CHECK BOTTOM POSITION FROM THE CURRENT BRICK */	                   
             
             if(pos1+1 < 6 && pos2 >= 0) {
                
-                
                 if(Min != undefined ) {
                 
                 if(Math.abs(temp_array[pos1][pos2] - temp_array[pos1+1][pos2]) < Min) {
@@ -88,7 +93,7 @@ function RatsPostions(input1,input2,input3)
                 }
             } 
             
-           /* Checking left position from current brick */ 
+           /* CHECK LEFT POSITION FROM THE CURRENT BRICK */ 
            
             if(pos1 >= 0 && pos2-1 >= 0 ) {
                 
@@ -101,26 +106,29 @@ function RatsPostions(input1,input2,input3)
                 
             } 
                 
-            return obj; /*Returning a next position of rat as an array of rats */
+            return obj; /* RETURN NEXT POSITION BASED ON THE MINIMUM TEMPRATURE DIFFERENCE */
         
-        } /*End of findMin function*/
+        } /*------------------------- END OF FINDMIN FUNCTION --------------------------------*/
             
         var count2=0;
         
-        /*No. of iteration need to perform to find the final rat position.*/ 
-        /*Taking input3 for looping constraint which passed as argument when ratsPosition function is called. */
+        /*NO. OF ITERATION NEED TO PERFORM TO FIND THE FINAL POSTION OF RATS .*/ 
+        /* TAKE INPUT3 FOR LOOPING CONSTRAINT WHICH PASSED AS ARGUMENT WHEN ratsPosition FUNCTION IS CALLED. */
+        
         for(var x=0;x<input3;x++)
             { 
                 if(count2 === 0 ) {
                     for(var q=0;q<input_arr.length;q++)   
                     {   
-                    	/*Array start from 0th position so we need to change the input */
+                    	/* ARRAY START FROM 0TH POSTION SO WE NEED TO CHANGE THE INPUT */
+                        
                         input_arr[q][0] = input_arr[q][0] - 1;
                         input_arr[q][1] = input_arr[q][1] - 1;
                         min_array.push(findMin(input_arr[q][0],input_arr[q][1]));
 
                     }
-                    count2++;
+                    count2++; /*ONLY ONCE WE NEED TO PEFORM -1 OPERATION */
+                    
                 } else {
                     
                     for(var q=0;q<input_arr.length;q++)   
@@ -129,14 +137,16 @@ function RatsPostions(input1,input2,input3)
 
                     }
                     
-                }
+                } /*END OF ELSE */
 
                 /*INPUT_ARR CONTAINS THE FINAL RESULT*/   
                 
-                input_arr = []; /*flusing the input_array for the next iteration */
+                input_arr = []; /* FLUSH THE INPUT ARRAY FOR THE NEXT ITERATION */
                 input_arr = min_array;
-                min_array = []; /*or flusing the min_array for the next iteration. */ 
-            }
+                min_array = []; /* FLUSH THE MIN_ARRAY FOR THE NEXT ITERATION. */ 
+            
+            	
+            } /*END OF FOR LOOP*/
            
            /*---------------------------------CREATE A STRING TO RETURN A FINAL RESULT-------------------*/
            var finalString = '';
@@ -146,12 +156,15 @@ function RatsPostions(input1,input2,input3)
               finalString += (input_arr[z][0] + 1) + '#' + (input_arr[z][1] + 1) + ','; 
               
            }
+           
+          /*RETURN THE FINAL OUTPUT*/ 
+          
           return finalString.substr(0,(finalString.length - 1));          
             
 	} 
 
 
-/*Calling a Function. */
+/* CALL FUNCTION TO CHECK THE RESULT */
 var finalRatsPositions = RatsPostions("1#1,2#5,3#3,6#3","2#6#8#6#-7,2#5#-5#-5#0,-1#3#-8#8#7,3#2#0#6#9,2#1#-4#5#8,-5#6#7#4#7",3);
 console.log(finalRatsPositions);
 
